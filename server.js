be869cfc0c06cf448e57249ff035e5579a465a4f
shtya <shtya54@gmail.com>
2025-08-05 20:15:43 +0300

    init

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const app = express();
 // تقديم الملفات الثابتة من مجلد البناء
 app.use(express.static(path.join(__dirname, 'build')));
 
-// إعادة توجيه جميع الطلبات إلى index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// // إعادة توجيه جميع الطلبات إلى index.html
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
-// تشغيل الخادم
+// // تشغيل الخادم
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
